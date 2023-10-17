2c00db3f637946fb8501c632ea67d114bd60e0e9
agneta1991 <pupienytea@gmail.com>
2023-10-17 11:04:46 +0300

    adding music album class

---
+++ musicalbum.rb
@@ -0,0 +1,18 @@
+require_relativev 'item'
+require 'date'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, archived, on_spotify: false)
+    super(publish_date, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    return false unless super
+    return false unless @on_spotify
+
+    true
+  end
+end
