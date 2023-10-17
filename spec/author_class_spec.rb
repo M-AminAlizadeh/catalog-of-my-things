3a87b2d037132698f5db76e379c326f354b48090
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-17 22:47:51 +0530

    add unit tests for author class

---
+++ spec/author_class_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../Games/author_class'
+require 'rspec'
+
+describe Author do
+    before(:each) do
+        @author = Author.new('FirstName', 'LastName')
+        @id = 1
+        @items = []
+    end
+
+    describe '#initialize constructor' do
+        it 'Checking Game instance' do
+            expect(@author).to be_an_instance_of(Author)
+        end
+
+        it 'Checking the first_name' do
+            expect(@author.first_name).to eq('FirstName')
+        end
+
+        it 'Checking the last_name' do
+            expect(@author.last_name).to eq('LastName')
+        end
+    end
+
+    describe '#add_item method' do
+        it 'should add the author to items' do
+            item = double('item')
+            allow(item).to receive(:author=)
+            @author.add_item(item)
+            expect(@author.items).to eq([item])
+        end
+    end
+end
