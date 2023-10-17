295ff8db5a44faa6dd7e9eebbec4f9c3d002d1ed
agneta1991 <pupienytea@gmail.com>
2023-10-17 14:24:46 +0300

    one test still fails

--- genre_test.rb
+++ genre_test.rb
@@ -2,15 +2,31 @@ require 'rspec'
 require_relative 'genre'
 
 describe Genre do
-  genre = Genre.new(269, 'Thriller')
-
   context 'attributes' do
     it 'has an id' do
+      genre = Genre.new(269, 'Thriller')
       expect(genre.id).to equal(269)
     end
 
     it 'has a name' do
+      genre = Genre.new(269, 'Thriller')
       expect(genre.name).to eq('Thriller')
+    end
+  end
+
+  context 'methods' do
+    it 'adds item to the array of items' do
+      genre = Genre.new(269, 'Thriller')
+      item = double('item')
+      expect(item).to receive(:genre=).with(genre)
+      genre.add_item(item)
+    end
+
+    it 'adds self in the array of items' do
+      genre = Genre.new(269, 'Thriller')
+      item = double('item')
+      genre.add_item(item)
+      expect(genre.items).to include(item)
     end
   end
 end
