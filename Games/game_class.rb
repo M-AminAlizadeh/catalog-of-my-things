f4b82bb47808449e794fbf60fb1a79645dc92f55
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-17 22:50:20 +0530

    fix linter errors

--- Games/game_class.rb
+++ Games/game_class.rb
@@ -3,13 +3,13 @@ require './item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at, publish_date: nil, archived: false)
+  def initialize(multiplayer, last_played_at, publish_date: nil)
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
-  #private
+  # private
 
   def can_be_archived?(item)
     super(item) && (Date.today - Date.parse(@last_played_at)).to_i / 365 >= 2
