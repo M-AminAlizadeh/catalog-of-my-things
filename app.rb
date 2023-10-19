6dab834cc78af7dc17dc621af8a87e885d870ba4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-19 09:55:08 +0330

    Add run method for all scenarios

--- app.rb
+++ app.rb
@@ -1,33 +1,40 @@
+require 'json'
+require_relative 'modules/book_module'
+require_relative 'modules/label_module'
+
 class App
-  attr_accessor :item, :books, :music, :games
+  include BookModule
+  include LabelModule
 
-  def initialize
-    @books = []
-    @music = []
-    @games = []
-  end
-
-  def list_all_books; end
-
-  def list_all_music_albums; end
-
-  def list_all_games; end
-
-  def list_all_music_genres; end
-
-  def list_all_labels; end
-
-  def list_authors; end
-
-  def add_book
-    # add a label
-  end
-
-  def add_music_album
-    # add a music genre
-  end
-
-  def add_game
-    # add an author
+  def run(option)
+    case option
+    when 1
+      list_of_books
+    when 2
+      list_of_music_album
+    when 3
+      list_of_games
+    when 4
+      list_of_genres
+    when 5
+      list_of_labels
+    when 6
+      list_of_authors
+    when 7
+      create_book
+    when 8
+      create_music_album
+    when 9
+      create_game
+    when 10
+      create_genre
+    when 11
+      create_label
+    when 12
+      create_author
+    else
+      puts 'Thanks for using this app'
+      exit
+    end
   end
 end
