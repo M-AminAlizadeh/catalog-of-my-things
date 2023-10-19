ba24090059c2a4281d571971a3e2b0573a458ff0
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-19 09:10:47 +0330

    Add json converter method to book class

--- classes/book.rb
+++ classes/book.rb
@@ -12,4 +12,18 @@ class Book < Item
   def can_be_archived?
     super || @cover_state == 'bad'
   end
+
+  def to_s
+    " Id: #{@id}, Published: #{@publish_date}, Publisher: #{@publisher}, Cover State: #{@cover_state}"
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'publisher' => @publisher,
+      'publish_date' => @publish_date,
+      'cover_state' => @cover_state
+    }.to_json(*args)
+  end
 end
