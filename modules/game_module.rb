00264128cdb816dd006b104ed59e024344ea50e3
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-19 05:36:22 +0530

    create GameModule with list and create methods

---
+++ modules/game_module.rb
@@ -0,0 +1,36 @@
+require_relative '../classes/game_class.rb'
+require_relative 'author_module'
+require 'json'
+
+module GameModule
+    include AuthorModule
+
+    def get_user_input(message)
+        print message
+        gets.chomp
+    end
+
+    def list_games
+        puts 'Listing Games records'
+        if @games.length == 0
+            puts "Game Records Found: 0"
+        else
+            @games.each_with_index do |item, index|
+                puts "#{index+1} - ID: #{item.id} Multiplayer: #{item.multiplayer}, last_played: #{item.last_played_at}, Game Published: #{item.publish_date}"
+            end
+        end
+    end
+
+    def create_game
+        puts 'Creating a Game Record: '
+        multiplayer = get_user_input('Multiplayer[Y/N]: ').casecmp('Y').zero?
+        last_played_at = get_user_input('Last played(yyyy-mm-dd): ')
+        publish_date = get_user_input('Game publish date(yyyy-mm-dd): ')
+        game = Game.new(multiplayer, last_played_at, publish_date)
+        @games.push(game)
+
+        add_author(game)
+
+        puts 'Game Added Successfully'
+    end
+end
